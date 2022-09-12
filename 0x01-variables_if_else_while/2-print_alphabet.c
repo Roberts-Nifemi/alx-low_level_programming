3151af7436ec067131adb3cb5d9f3c4d3cea0871
Nifemi Roberts <nifemi.nr@gmail.com>
2022-09-12 14:41:31 -0700

    I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,17 +3,18 @@
 /**
  * main - prints the alphabet in lowercase
  * Return : Always 0 (success)
- * */
+ */
 
 int main(void)
 {
 	int i = 'a';
-	while (i<= 'z')
+
+	while (i <= 'z')
 	{
 		putchar(i);
 		i += 1;
 	}
-	putchar('\n');
+	putchar ('\n');
 	return(0);
 }
 
