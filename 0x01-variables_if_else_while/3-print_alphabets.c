87b234b9fa2ac7a8c0be6e6f8405040e65d24b41
Nifemi Roberts <nifemi.nr@gmail.com>
2022-09-12 17:44:48 -0700

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,17 +11,17 @@ int main(void)
 	int lw = 'a';
 
 	int up = 'A';
-	
+
 	while (lw <= 'z')
 	{
 		putchar(lw);
 
-		lw +=1;
+		lw += 1;
 	}
 	while (up <= 'Z')
 	{
 		putchar(up);
-		up +=1;
+		up += 1;
 	}
 	putchar('\n');
 	return (0);
